a07557b3fb8c5b1786ca2e27a8b543ed1a4c12af
ART89-prog <dykpgilkom@mail.ru>
2024-03-09 19:41:48 +0300

    256245653524524626

--- js/scripts.js
+++ js/scripts.js
@@ -87,6 +87,17 @@ $(() => {
 
 
 
+	let header = $('header');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1) {
+			header.addClass('header_fixed');
+		} else {
+			header.removeClass('header_fixed');
+		}
+	});
+
+
+
 	$(document).ready(function () {
 		$('.table-responsive-stack').find("th").each(function (i) {
 
